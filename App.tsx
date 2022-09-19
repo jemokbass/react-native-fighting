00f107a046d5060968f5f26474181d26c15763f7
jkakhovtsev <jkakhovtsev@emphasoft.com>
2022-09-20 01:37:29 +0300

    Initialising project with react-native init typescript and uninstall carbage.

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <Text>Test</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
